8186358fb7948a773f10ad37f8b363bfb2027e37
sandeshs0 <hellosandesh0@gmail.com>
2024-12-20 19:13:03 +0545

    app theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tutorme/core/app_theme/app_theme.dart';
 import 'package:tutorme/view/splash_view.dart';
 
 void main() {
@@ -10,9 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: const SplashScreen(),
+      theme: getApplicationTheme(),
     );
   }
 }
---
+++ lib/core/app_theme/app_theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    primarySwatch: Colors.blue,
+    scaffoldBackgroundColor: const Color(0xfff5f8ff),
+    fontFamily: 'Monsterrat Regular',
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      // style: ElevatedButton.styleFrom(
+      style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          minimumSize: const Size(double.infinity, 50),
+          backgroundColor: const Color.fromARGB(255, 0, 94, 255),
+          textStyle: const TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 255, 255, 255)),
+          foregroundColor: Colors.white),
+      // backgroundColor: const Color(0xff3f51b5),
+      // shape: RoundedRectangleBorder(
+      //   borderRadius: BorderRadius.circular(10),
+      // ),
+      // textStyle: const TextStyle(
+      //     fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white)),
+    ),
+  );
+}
