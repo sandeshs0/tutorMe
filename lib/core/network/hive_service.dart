9d90a06d012daf7875a00a1e9b9188b11cd8b7a6
sandeshs0 <hellosandesh0@gmail.com>
2025-01-17 12:05:54 +0545

    adapter registered in hive service

--- lib/core/network/hive_service.dart
+++ lib/core/network/hive_service.dart
@@ -1,8 +1,61 @@
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:tutorme/features/auth/data/model/auth_hive_model.dart';
 
-class LocalNetwork {
+class HiveService {
   static Future<void> init() async {
     // Initialize the database
     var directory = await getApplicationDocumentsDirectory();
+    var path='${directory.path}/tutorme.db';
+    Hive.init(path);
+    // Register Adapters
+    Hive.registerAdapter(UserHiveModelAdapter());
+  }
+
+  // User Queries
+  // Register
+  Future<void> addUser(UserHiveModel user)async{
+    var box = await Hive.openBox<UserHiveModel>('userBox');
+    await box.put(user.id, user);
+  }
+
+  // Delete a User
+   Future<void> deleteUser(String id) async {
+    var box = await Hive.openBox<UserHiveModel>('userBox');
+    await box.delete(id);
+  }
+
+  // get all
+    Future<List<UserHiveModel>> getAllUsers() async {
+    var box = await Hive.openBox<UserHiveModel>('userBox');
+    return box.values.toList();
+  }
+
+    // Find User by Email and Password (Login)
+  Future<UserHiveModel?> login(String email, String password) async {
+    var box = await Hive.openBox<UserHiveModel>('userBox');
+    try {
+      return box.values.firstWhere(
+        (user) => user.email == email && user.password == password,
+      );
+    } catch (e) {
+      return null; // Return null if no user is found
+    }
+  }
+
+  
+  // Clear All Data
+  Future<void> clearAll() async {
+    await Hive.deleteBoxFromDisk('userBox');
+  }
+
+  // Clear Specific User Box
+  Future<void> clearUserBox() async {
+    await Hive.deleteBoxFromDisk('userBox');
+  }
+
+  // Close Hive Database
+  Future<void> close() async {
+    await Hive.close();
   }
 }
