3e04ca253eb8d437d257ece89ac489e8f62da285
sandeshs0 <hellosandesh0@gmail.com>
2025-01-30 22:31:00 +0545

    login use case for token storage in shared pref

--- lib/features/auth/domain/use_case/login_usecase.dart
+++ lib/features/auth/domain/use_case/login_usecase.dart
@@ -1,10 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:tutorme/app/shared_prefs/token_shared_prefs.dart';
 import 'package:tutorme/app/usecase/usecase.dart';
 import 'package:tutorme/core/error/failure.dart';
 import 'package:tutorme/features/auth/domain/repository/auth_repository.dart';
 
-class LoginParams extends Equatable{
+class LoginParams extends Equatable {
   final String email;
   final String password;
 
@@ -14,23 +16,35 @@ class LoginParams extends Equatable{
   });
 
   const LoginParams.initial()
-      :email='',
-      password='';
-      
-        @override
-        // TODO: implement props
-        List<Object?> get props => [email,password];
-      
+      : email = '',
+        password = '';
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [email, password];
 }
 
-class LoginUsecase implements UsecaseWithParams<String,LoginParams>{
+class LoginUsecase implements UsecaseWithParams<String, LoginParams> {
   final IAuthRepository repository;
+  final TokenSharedPrefs tokenSharedPrefs;
 
-  LoginUsecase(this.repository);
+  LoginUsecase(this.repository, this.tokenSharedPrefs);
 
   @override
   Future<Either<Failure, String>> call(LoginParams params) {
-    return repository.loginUser(params.email, params.password);
+    // Save token in Shared Preferences
+    return repository.loginUser(params.email, params.password).then((value) {
+      return value.fold(
+        (failure) => Left(failure),
+        (token) {
+          tokenSharedPrefs.saveToken(token);
+          tokenSharedPrefs.getToken().then((value) {
+            debugPrint("Token is$value");
+          });
+          print(tokenSharedPrefs);
+          return Right(token);
+        },
+      );
+    });
   }
-  
 }
