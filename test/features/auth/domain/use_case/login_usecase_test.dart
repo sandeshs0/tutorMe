322f443fc5d77eb9f27253099866958f213c73eb
sandeshs0 <hellosandesh0@gmail.com>
2025-02-07 13:02:58 +0545

    login test: invalid credentials and token prefs

---
+++ test/features/auth/domain/use_case/login_usecase_test.dart
@@ -0,0 +1,69 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:tutorme/core/error/failure.dart';
+import 'package:tutorme/features/auth/domain/use_case/login_usecase.dart';
+
+import 'repository.mock.dart';
+import 'token.mock.dart';
+
+void main() {
+  late MockAuthRepository repository;
+  late LoginUsecase usecase;
+  late MockTokenSharedPrefs tokenSharedPrefs;
+
+  setUp(() {
+    repository = MockAuthRepository();
+    tokenSharedPrefs = MockTokenSharedPrefs();
+    usecase = LoginUsecase(repository, tokenSharedPrefs);
+    // registerFallbackValue(BatchEntity.empty());
+  });
+
+  const userLoginParams =
+      LoginParams(email: "hellosandesh0@gmail.com", password: "password");
+
+  const generatedToken = "mock_jwt_token";
+
+  group('Login Usecase Tests', () {
+    test('login fails when incorrect credentials', () async {
+      // Arrange
+      when(() => repository.loginUser(any(), any())).thenAnswer(
+          (_) async => const Left(ApiFailure(message: "Invalid Credentials")));
+
+      // Act
+      final result = await usecase(userLoginParams);
+
+      // Assert
+      expect(result, const Left(ApiFailure(message: "Invalid Credentials")));
+      verify(() => repository.loginUser(any(), any())).called(1);
+      verifyNever(() => tokenSharedPrefs.saveToken(any()));
+    });
+
+    test('returns token when login is successful', () async {
+      // Arrange
+      when(() => repository.loginUser(any(), any()))
+          .thenAnswer((_) async => const Right(generatedToken));
+      when(() => tokenSharedPrefs.saveToken(any()))
+          .thenAnswer((_) async => const Right(null));
+      when(() => tokenSharedPrefs.getToken())
+          .thenAnswer((_) async => const Right(generatedToken));
+
+      // Act
+      final result = await usecase(userLoginParams);
+
+      // Assert
+      expect(result, const Right(generatedToken));
+      verify(() => repository.loginUser(
+          userLoginParams.email, userLoginParams.password)).called(1);
+      verify(() => tokenSharedPrefs.saveToken(generatedToken)).called(1);
+      verify(() => tokenSharedPrefs.getToken()).called(1);
+    });
+
+    test('should fail when email is empty', () async {
+      // Arrange
+      const userLoginParams = LoginParams(email: '', password: 'password');
+      when(() => repository.loginUser(any(), any())).thenAnswer((_) async =>
+          const Left(ApiFailure(message: "Email cannot be empty")));
+    });
+  });
+}
---
+++ test/features/auth/domain/use_case/repository.mock.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:tutorme/features/auth/domain/repository/auth_repository.dart';
+
+class MockAuthRepository extends Mock implements IAuthRepository {}
