c4cf31c2202f814e37d9af24834494121d9f83c6
sandeshs0 <hellosandesh0@gmail.com>
2025-02-07 11:50:59 +0545

    cleaned code

--- lib/app/constants/api_endpoints.dart
+++ lib/app/constants/api_endpoints.dart
@@ -1,6 +1,5 @@
 class ApiEndpoints {
   ApiEndpoints._();
-
   static const Duration connectionTimeout= Duration(seconds:1000);
   static const Duration receiveTimeout = Duration(seconds: 1000);
   static const String baseUrl = "http://10.0.2.2:3000/";
